a3eed5e9ad3f96b7cc89848eeece6c5fc3ad34b7
Neeraj Paliwal <43784482+Nirajpaliwal@users.noreply.github.com>
2025-08-03 18:03:09 +0530

    Fix: TypeError in add function by converting inputs to int

--- PatchIQAI_App/Agent Test Code/buggy_code.py
+++ PatchIQAI_App/Agent Test Code/buggy_code.py
@@ -1,19 +1,15 @@
-def add(data):
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    result = num1 + num2
-  
-    return {
-        'result': result,
-        'num1': num1,
-        'num2': num2
-    }
+def add(num1, num2):
+    result = int(num1) + int(num2)
+    return result
 
-data = {
-    'num1': '5',
-    'num2': 10
-}   
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
 
-added_result = add(data)
-print("Added Result:", added_result)
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
