85eeb16a94d2a5df5716855deb3d61de1784c4ba
Neeraj Paliwal <43784482+Nirajpaliwal@users.noreply.github.com>
2025-08-05 23:55:52 +0530

    Fix: TypeError in add function by casting inputs to int

--- PatchIQAI_App/Agent Test Code/buggy_code.py
+++ PatchIQAI_App/Agent Test Code/buggy_code.py
@@ -1,19 +1,5 @@
-def add(data):
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-    
-    result = num1 + num2
-  
-    return {
-        'result': result,
-        'num1': num1,
-        'num2': num2
-    }
+def add(num1, num2):
+    result = int(num1) + int(num2)
+    return result
 
-data = {
-    'num1': '5',
-    'num2': 10
-}   
-
-added_result = add(data)
-print("Added Result:", added_result)
+print(add("1", 2))
