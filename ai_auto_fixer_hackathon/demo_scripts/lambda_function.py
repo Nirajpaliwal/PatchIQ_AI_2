87d2675672396f88f5bc384db84bb04bf10624e0
Neeraj Paliwal <43784482+Nirajpaliwal@users.noreply.github.com>
2025-08-15 21:31:51 +0530

    Create lambda_function.py

---
+++ ai_auto_fixer_hackathon/demo_scripts/lambda_function.py
@@ -0,0 +1,20 @@
+def add(data):
+    num1 = data.get('num1')
+    num2 = data.get('num2')
+    
+    result = num1 + num2
+
+    return {
+        'result': result,
+        'num1': num1,
+        'num2': num2
+    }
+
+
+data = {
+    'num1': '5',
+    'num2': 10
+}   
+
+added_result = add(data)
+print("Added Result:", added_result)
